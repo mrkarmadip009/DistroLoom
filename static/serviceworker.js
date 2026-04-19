ddd2990ccb4a824f4e7afdd52b52372860b27f27
mrkarmadip009 <alpanena94994@gmail.com>
2026-04-19 17:53:42 +0530

    removed for app

--- static/serviceworker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(e) {
-    console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', function(event) {
-    // basic fetch (no offline yet)
-});
